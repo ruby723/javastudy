290483c0937a2c20734d7b19c68e629fe0bf48e3
ㅠㅑㅅ <@DESKTOP-D4S303H>
2021-04-30 15:55:57 +0900

    java collection

---
+++ chapter04/src/main/java/collenction/StackTest.java
@@ -0,0 +1,27 @@
+package collenction;
+
+import java.util.Stack;
+
+public class StackTest {
+	
+	public static void main(String[] args) {
+		
+		Stack<String> s = new Stack<>();
+		
+		while(!s.isEmpty()) {
+			String str=s.pop();
+			System.out.println(str);
+		}
+		
+		//비어있는 경우엔 예외 발생
+		
+		
+		s.push("둘리");
+		s.push("마이콜");
+		s.push("도우너");
+		
+		System.out.println(s.pop());
+		System.out.println(s.peek());
+		System.out.println(s.pop());
+	}
+}
