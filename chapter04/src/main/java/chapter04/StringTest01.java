881056687d98791cfd4b3788c4ca687941ef27fe
ㅠㅑㅅ <@DESKTOP-D4S303H>
2021-04-30 09:51:25 +0900

    String Test

---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,15 @@
+package chapter04;
+
+public class StringTest01 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("c:\rtemp");
+		
+		// \t : tab
+		// \r : carriage return
+		// \n : newline
+		// \b : beeeep
+		
+	}
+}
