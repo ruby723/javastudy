32dfd421ada745edbaca8c0f0a25a121d5336766
ㅠㅑㅅ <@DESKTOP-D4S303H>
2021-04-30 14:07:52 +0900

    .

--- chapter03/src/main/java/chapter03/MyClass.java
+++ chapter03/src/main/java/chapter03/MyClass.java
@@ -1,5 +1,17 @@
 package chapter03;
 
 public class MyClass {
-
+	private static MyClass instance;
+	
+	private MyClass() {
+		
+	}
+	
+	//Singleton+Factory Method
+	public static MyClass getInstance() {
+		if(instance == null) {
+			instance=new MyClass();
+		}
+		return instance;
+	}
 }
--- chapter03/src/main/java/chapter03/MyClassTest.java
+++ chapter03/src/main/java/chapter03/MyClassTest.java
@@ -3,8 +3,10 @@ package chapter03;
 public class MyClassTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MyClass myClass01=MyClass.getInstance();
+		MyClass myClass02=MyClass.getInstance();
+		
+		System.out.println(myClass01==myClass02);
 	}
 
 }
